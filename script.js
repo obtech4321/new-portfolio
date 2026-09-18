ddd97a9d9bb96f17d6f64c78a244d78b020e164f
obtech4321 <obafemi.isola@gmail.com>
2026-09-18 12:07:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ //Dark || Light mode
+
+let themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+});
